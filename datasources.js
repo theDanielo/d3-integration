922a0741eb2242cd3bac4853bd4b7463bff7f26e
Daniel Gracia <danigr@uio.no>
2025-10-29 15:25:30 +0100

    new change

--- datasources.js
+++ datasources.js
@@ -1,9 +1,1 @@
-'use strict';
-
-// Wrap everything in an anonymous function to avoid polluting the global namespace
-(function () {
-  $(document).ready(function () {
-    console.log("hola nene")
-  })();
- })
-
+console.log("hahhhahha")
